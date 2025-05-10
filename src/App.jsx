2b6e264bd58c7c167d5d253c26a5fb0ab288f1a8
Sara <sarailic431@gmail.com>
2025-05-10 20:36:45 +0200

    first part of css done

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,33 @@ function App() {
           <div class="cart-content-number">0</div>
         </div>
       </nav>
-      <section class="section"></section>
+      <section class="section">
+        <div class="products-cont">
+          <label class="search-label">Search Product</label> <br></br>
+          <input type="text" class="input-product" />
+          <div class="products-list">
+            <article class="product-item">
+              <div class="product-info">
+                <h4>Sushi</h4>
+                <p>Finest fish and veggies</p>
+                <span>22.99$</span>
+              </div>
+              <div class="product-amount">
+                <div class="input-amount-cont">
+                  <label>Amount</label>
+                  <input class="input-amount" type="number" value="1" />
+                </div>
+                <button class="add-btn">ADD +</button>
+              </div>
+            </article>
+          </div>
+          <button class="go-to-cart-btn">Go to Your cart</button>
+          <div class="total-price">
+            <h4>Total Price:</h4>
+            <span>$12345</span>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
