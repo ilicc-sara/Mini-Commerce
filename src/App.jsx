3bbcc38c1d8f8f053d002cfbb888df5d6be3bab5
Sara <sarailic431@gmail.com>
2025-05-10 17:36:50 +0200

    making navigation css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <h2>Mini Commerce</h2>
+
+        <div class="cart-notification">
+          <ion-icon name="cart-outline" class="cart-icon"></ion-icon>
+          <div class="cart-content-number">0</div>
+        </div>
+      </nav>
+      <section></section>
+    </>
+  );
 }
 
 export default App;
